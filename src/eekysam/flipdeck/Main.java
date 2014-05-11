7164d75b959bcece57f7023142f9c63f20c49d74
Sam Sartor <eeky.sam@gmail.com>
2014-05-11 13:24:01 -0600

    Data
    Ran program for 1000 decks and 10000 decks

--- src/eekysam/flipdeck/Main.java
+++ src/eekysam/flipdeck/Main.java
@@ -1,13 +1,108 @@
 package eekysam.flipdeck;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		for (int i = 1; i <= 1000; i++)
+		int min = 1;
+		int max = 1000;
+		BufferedWriter writer = null;
+		String fname = null;
+
+		if (args.length == 1)
 		{
-			Deck d = Deck.newInstance(i);
-			System.out.println(d.size + "\t" + d.rotateNumber());
+			try
+			{
+				min = 1;
+				max = Integer.parseInt(args[0]);
+				fname = null;
+			}
+			catch (NumberFormatException e)
+			{
+				min = 1;
+				max = 1000;
+				fname = args[0];
+			}
+		}
+		else if (args.length == 2)
+		{
+			try
+			{
+				max = Integer.parseInt(args[1]);
+				min = Integer.parseInt(args[0]);
+				fname = null;
+			}
+			catch (NumberFormatException e0)
+			{
+				min = 1;
+				max = Integer.parseInt(args[0]);
+				fname = args[1];
+			}
+		}
+		else if (args.length >= 3)
+		{
+			max = Integer.parseInt(args[1]);
+			min = Integer.parseInt(args[0]);
+			fname = args[2];
+		}
+
+		if (fname != null)
+		{
+			try
+			{
+				File file = new File(fname);
+				file.createNewFile();
+				writer = new BufferedWriter(new FileWriter(file));
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		if (writer == null)
+		{
+			for (int i = min; i <= max; i++)
+			{
+				Deck d = Deck.newInstance(i);
+				System.out.println(d.size + "\t" + d.rotateNumber());
+			}
+		}
+		else
+		{
+			try
+			{
+				int delta = 0;
+				for (int i = min; i <= max; i++)
+				{
+					Deck d = Deck.newInstance(i);
+					writer.append(d.size + "\t" + d.rotateNumber() + "\n");
+					if (delta >= 500)
+					{
+						delta = 0;
+						System.out.println((int)((((float) (i - min)) / (max - 1)) * 100) + "%");
+					}
+					delta++;
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			System.out.println("100% - Done!");
+			try
+			{
+				writer.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 }
